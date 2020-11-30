8de48cd61b9988d62678c161bce0a104d7d2ffec
Sarah Mayoral-Osmolski <sarah@semolski.com>
2020-11-30 17:44:25 -0600

    Github React Router fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   render() {
     return (
         <Provider store={store}>
-            <BrowserRouter>
+            <BrowserRouter basename="semolski.github.io">
                 <div>
                     <Main />
                 </div>
