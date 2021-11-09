b4fabc629b2bd7d8e7799fa47fb519293b1d123d
Sarah Mayoral-Osmolski <sarah@semolski.com>
2021-11-08 18:06:27 -0600

    Refactored user.actions to include favorites

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,30 +1,41 @@
-import * as ActionTypes from "../ActionTypes";
+import UserActionTypes from "./user.types";
 import {baseUrl} from "../../shared/baseUrl";
-import {favoritesFailed, fetchFavorites} from "../ActionCreators";
+import {favoritesFailed, fetchFavorites} from "../favorite/favorites.actions";
 
 export const requestLogin = (creds) => {
     return {
-        type: ActionTypes.LOGIN_REQUEST,
+        type: UserActionTypes.LOGIN_REQUEST,
         creds
     }
 }
 
 export const receiveLogin = (response) => {
     return {
-        type: ActionTypes.LOGIN_SUCCESS,
+        type: UserActionTypes.LOGIN_SUCCESS,
         token: response.token
     }
 }
 
 export const loginError = (message) => {
     return {
-        type: ActionTypes.LOGIN_FAILURE,
+        type: UserActionTypes.LOGIN_FAILURE,
         message
     }
 }
 
+export const requestLogout = () => {
+    return {
+        type: UserActionTypes.LOGOUT_REQUEST
+    }
+}
+
+export const receiveLogout = () => {
+    return {
+        type: UserActionTypes.LOGOUT_SUCCESS
+    }
+}
+
 export const loginUser = (creds) => (dispatch) => {
-    // We dispatch requestLogin to kickoff the call to the API
     dispatch(requestLogin(creds))
 
     return fetch(baseUrl + 'users/login', {
@@ -52,7 +63,6 @@ export const loginUser = (creds) => (dispatch) => {
                 // If login was successful, set the token in local storage
                 localStorage.setItem('token', response.token);
                 localStorage.setItem('creds', JSON.stringify(creds));
-                // Dispatch the success action
                 dispatch(fetchFavorites());
                 dispatch(receiveLogin(response));
             }
@@ -65,17 +75,7 @@ export const loginUser = (creds) => (dispatch) => {
         .catch(error => dispatch(loginError(error.message)))
 };
 
-export const requestLogout = () => {
-    return {
-        type: ActionTypes.LOGOUT_REQUEST
-    }
-}
 
-export const receiveLogout = () => {
-    return {
-        type: ActionTypes.LOGOUT_SUCCESS
-    }
-}
 
 // Logs the user out
 export const logoutUser = () => (dispatch) => {
